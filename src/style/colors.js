1aacccbdf4a280351e4729ca4e4fd5c9147231ff
Tralyn Le <let1@unlv.nevada.edu>
2018-04-07 06:58:54 +0800

    Stylesheet of color palette for UI

---
+++ src/style/colors.js
@@ -0,0 +1,25 @@
+'use strict'
+const colors = {
+  txtMain: '#00a699',
+  txtMainBlue: '#37B8CB',
+  txtDescription: '#757575',
+  txtDark: '#484848',
+  txtWhite: '#ffffff',
+  txtError: '#fb642d',
+  txtSuccess: '#25ce66',
+
+  bgMain: '#00a699',
+  bgMainDark: '#007e8c', // For screens > auth
+  bgMainBlue: '#37B8CB',
+  bgWhite: '#ffffff',
+  bgError: '#fb642d',
+  bgChat: '#f1f1f1',
+
+  bdMainBlue: '#37B8CB',
+  bdWhite: '#ffffff',
+  bdLine: '#dddddd',
+  bdInput: '#cbcbcb',
+
+}
+
+export default colors
