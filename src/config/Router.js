2bcc706c19c1ad303dfe29c99c933f9791a2ba31
johnalexiv <johnalexiv@u-10-81-142-191.dhcp.unlv.edu>
2018-04-19 18:17:11 -0700

    spliting sign up and sign in pages

--- src/config/Router.js
+++ src/config/Router.js
@@ -4,6 +4,7 @@ import LoginScreen from '../screens/LoginScreen';
 import EmailLogin from '../screens/EmailLogin';
 import MainScreen from '../screens/MainScreen';
 import privacypolicy from '../screens/privacypolicy';
+import ActualLoginScreen from '../screens/ActualLoginScreen';
 
 export const LoginStack = StackNavigator({
     LoginScreen: {
@@ -23,6 +24,9 @@ export default Root = StackNavigator({
     },
     privacypolicy: {
         screen: privacypolicy,
+    },
+    ActualLoginScreen: {
+        screen: ActualLoginScreen,
     },
 },  {
     mode: 'card',
