26117068c9680c818cd80c73f59910ce46f4eaf9
rayuste <ayuste@unlv.nevada.edu>
2018-04-30 09:49:36 -0700

    formatted about us

--- src/screens/AboutUs.js
+++ src/screens/AboutUs.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { View, StyleSheet,Image, Text, ScrollView} from 'react-native';
 import { Constants } from 'expo';
-import { Header, Button,Left, Icon } from 'native-base';
+import Icon from 'react-native-vector-icons/Feather';
 
 
 
@@ -14,32 +14,30 @@ export default class AboutUs extends React.Component {
   }
   render() {
     return (
-      <View style={styles.container}>
+      <View style = {styles.container}>
+      <View style={styles.backContainer}>
+          <View style={styles.back}>
+              <Icon name="chevron-left" size={40} color="#32a9ba" 
+                  onPress={this.onBackButton}/>
+          </View>
+      </View>
+
         <ScrollView>
-        <Header style = {{backgroundColor: 'white'}}>
-        <Left>
-        <Button transparent>
-        <Icon name="ios-arrow-back" size={40} color="#32a9ba"
-          onPress={this.onBackButton}/>
-        </Button>
-        </Left>
-          <Button transparent>
-            <Image source={require('../images/FMicon.png')}   style = {styles.headerButton} /*onPress = {}*/ />
-          </Button>
-        </Header>
+
         <Text style = {styles.title}>About Us</Text>
-        <Text style={{fontSize:20, marginTop:10, textAlignVertical:'center', textAlign: 'center', color: '#4d4d4d'}} >According to the ASPCA, animal shelter overpopulation leads to 1.5 million euthanized animals per year, yet breeders are the top source for animal purchases. Breeders are more expensive and mired in allegations of cruelty and neglect. When purchasing from a breeder, it is unknown whether the conditions are humane. Adopting from shelters is different in that they are qualified and accountable for the treatment of their animals but searching individual shelters can be difficult and time consuming. </Text>
-        <Image source={require('../images/cageddogs.jpg')}   style = {{width:250, height: 250, marginLeft:65,}} />
+        <Text style={styles.body} >According to the ASPCA, animal shelter overpopulation leads to 1.5 million euthanized animals per year, yet breeders are the top source for animal purchases. Breeders are more expensive and mired in allegations of cruelty and neglect. When purchasing from a breeder, it is unknown whether the conditions are humane. Adopting from shelters is different in that they are qualified and accountable for the treatment of their animals but searching individual shelters can be difficult and time consuming. </Text>
+        <Image source={require('../images/cageddogs.jpg')}   style = {styles.image} />
 
-         <Text style={{fontSize:20, marginTop:10, textAlignVertical:'center', textAlign: 'center', color: '#4d4d4d' }}>Our company is a team of individuals passionate about animal care who are dedicated to helping animals around the country find loving forever homes. We are looking to ease the burden and simplify the process of pet adoption in hopes of increasing adoption through shelters. Our goal is to connect you to reputable shelters anywhere in the U.S. through the comfort of your phone. </Text>
+         <Text style={styles.body}>Our company is a team of individuals passionate about animal care who are dedicated to helping animals around the country find loving forever homes. We are looking to ease the burden and simplify the process of pet adoption in hopes of increasing adoption through shelters. Our goal is to connect you to reputable shelters anywhere in the U.S. through the comfort of your phone. </Text>
 
-        <Image source={require('../images/happydog.jpg')}   style = {{width:250, height: 250, marginLeft:65,}} />
+        <Image source={require('../images/happydog.jpg')}   style = {styles.image} />
 
-          <Text style={{fontSize:20, marginTop:10, textAlignVertical:'center', textAlign: 'center', color: '#4d4d4d' }}>Let us help you find your fluffy soulmate! Use the app that allows you to connect to the pet of your dreams anywhere in the country. Just log in, create an account, enter the pet you’re looking for and we’ll help you find the perfect addition to your family. With FluffyMates you will instantly connect to loving pets in need of a forever home anywhere in the country for free. Find love, save lives, end cruelty . . . Adopt, Don’t Shop</Text>
+          <Text style={styles.body}>Let us help you find your fluffy soulmate! Use the app that allows you to connect to the pet of your dreams anywhere in the country. Just log in, create an account, enter the pet you’re looking for and we’ll help you find the perfect addition to your family. With FluffyMates you will instantly connect to loving pets in need of a forever home anywhere in the country for free. Find love, save lives, end cruelty . . . Adopt, Don’t Shop</Text>
 
-            <Image source={require('../images/adoptabledog.jpg')}   style = {{width:250, height: 250, marginLeft:65,}} />
+            <Image source={require('../images/adoptabledog.jpg')}   style = {styles.image} />
 
         </ScrollView>
+        
       </View>
     );
   }
@@ -53,19 +51,43 @@ const styles = StyleSheet.create({
     paddingTop: Constants.statusBarHeight,
     backgroundColor: 'white',
   },
-  headerButton:
-  {
-  width:60,
-  height:60,
-  marginTop: 0,
-  marginRight:150
+  container: {
+    flex: 1,
+    paddingTop: 35,
   },
-  title:
-  {
-      marginTop: 10,
-      fontSize: 50,
-      textAlign: 'center'
+  back: {
+    flex: 1,
   },
-
+  backContainer: {
+    height: 45,
+    flexDirection: 'row',
+    borderWidth: 1,
+    borderTopWidth: 0,
+    borderLeftWidth: 0,
+    borderRightWidth: 0,
+    borderBottomColor: 'rgba(0,0,0,0.05)',
+  },
+  title:{
+    marginTop: 10,
+    fontSize: 30,
+    textAlign: 'center'
+  },
+  body: {
+    fontSize:13,
+    marginTop:10,
+    marginLeft:15,
+    marginRight: 15,
+    textAlign:'justify',
+    color: '#4d4d4d'
+  },
+  image: {
+    marginTop: 10,
+    marginLeft: 15,
+    marginRight: 15,
+    width: 300,
+    height: 300,
+    alignSelf: 'center',
+    
+  }
 
 });
