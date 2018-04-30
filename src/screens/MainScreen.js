43c62e629789d3e5869ab916b1f375b569f7fc22
nylart <let1@unlv.nevada.edu>
2018-04-30 13:06:20 -0700

    Receive cards through json directly on Main Screen

--- src/screens/MainScreen.js
+++ src/screens/MainScreen.js
@@ -110,68 +110,16 @@ class Card extends React.Component {
 
 export default class MainScreen extends React.Component {
     constructor(props) {
-        super(props);
+        super(props)
 
-        if (filters.species == "D") {
-            this.state = {
-              cards: [
-                //{id: `filters.index`, name: `${pets.dogs[filters.index].name}`, breed: `${pets.dogs[filters.index].breed}`, distance: `${pets.dogs[filters.index].radius}`, source: `${pets.dogs[filters.index].photo}`},
-                {id: `D00`, name: `${pets.dogs[0].name}`, breed: `${pets.dogs[0].breed}`, distance: `${pets.dogs[0].radius}`, source: `${pets.dogs[0].photo}`},
-                {id: `D01`, name: `${pets.dogs[1].name}`, breed: `${pets.dogs[1].breed}`, distance: `${pets.dogs[1].radius}`, source: `${pets.dogs[1].photo}`},
-                {id: `D02`, name: `${pets.dogs[2].name}`, breed: `${pets.dogs[2].breed}`, distance: `${pets.dogs[2].radius}`, source: `${pets.dogs[2].photo}`},
-                {id: `D03`, name: `${pets.dogs[3].name}`, breed: `${pets.dogs[3].breed}`, distance: `${pets.dogs[3].radius}`, source: `${pets.dogs[3].photo}`},
-                {id: `D04`, name: `${pets.dogs[4].name}`, breed: `${pets.dogs[4].breed}`, distance: `${pets.dogs[4].radius}`, source: `${pets.dogs[4].photo}`},
-                {id: `D05`, name: `${pets.dogs[5].name}`, breed: `${pets.dogs[5].breed}`, distance: `${pets.dogs[5].radius}`, source: `${pets.dogs[5].photo}`},
-                {id: `D06`, name: `${pets.dogs[6].name}`, breed: `${pets.dogs[6].breed}`, distance: `${pets.dogs[6].radius}`, source: `${pets.dogs[6].photo}`},
-                {id: `D07`, name: `${pets.dogs[7].name}`, breed: `${pets.dogs[7].breed}`, distance: `${pets.dogs[7].radius}`, source: `${pets.dogs[7].photo}`},
-                {id: `D08`, name: `${pets.dogs[8].name}`, breed: `${pets.dogs[8].breed}`, distance: `${pets.dogs[8].radius}`, source: `${pets.dogs[8].photo}`},
-                {id: `D09`, name: `${pets.dogs[9].name}`, breed: `${pets.dogs[9].breed}`, distance: `${pets.dogs[9].radius}`, source: `${pets.dogs[9].photo}`},
-                {id: `D10`, name: `${pets.dogs[10].name}`, breed: `${pets.dogs[10].breed}`, distance: `${pets.dogs[10].radius}`, source: `${pets.dogs[10].photo}`},
-                {id: `D11`, name: `${pets.dogs[11].name}`, breed: `${pets.dogs[11].breed}`, distance: `${pets.dogs[11].radius}`, source: `${pets.dogs[11].photo}`},
-                {id: `D12`, name: `${pets.dogs[12].name}`, breed: `${pets.dogs[12].breed}`, distance: `${pets.dogs[12].radius}`, source: `${pets.dogs[12].photo}`},
-                {id: `D13`, name: `${pets.dogs[13].name}`, breed: `${pets.dogs[13].breed}`, distance: `${pets.dogs[13].radius}`, source: `${pets.dogs[13].photo}`},
-                {id: `D14`, name: `${pets.dogs[14].name}`, breed: `${pets.dogs[14].breed}`, distance: `${pets.dogs[14].radius}`, source: `${pets.dogs[14].photo}`},
-              ],
-
-          list: [],
-          refreshing: false,
-          currentPet: null,
-            };
-          } else if (filters.species  == "C" ){
-              // for cats
-              this.state = {
-                cards: [
-                  {id: `C00`, name: `${pets.cats[0].name}`, breed: `${pets.cats[0].breed}`, distance: `${pets.cats[0].radius}`, source: `${pets.cats[0].photo}`},
-                  {id: `C01`, name: `${pets.cats[1].name}`, breed: `${pets.cats[1].breed}`, distance: `${pets.cats[1].radius}`, source: `${pets.cats[1].photo}`},
-                  {id: `C02`, name: `${pets.cats[2].name}`, breed: `${pets.cats[2].breed}`, distance: `${pets.cats[2].radius}`, source: `${pets.cats[2].photo}`},
-                  {id: `C03`, name: `${pets.cats[3].name}`, breed: `${pets.cats[3].breed}`, distance: `${pets.cats[3].radius}`, source: `${pets.cats[3].photo}`},
-                  {id: `C04`, name: `${pets.cats[4].name}`, breed: `${pets.cats[4].breed}`, distance: `${pets.cats[4].radius}`, source: `${pets.cats[4].photo}`},
-                  {id: `C05`, name: `${pets.cats[5].name}`, breed: `${pets.cats[5].breed}`, distance: `${pets.cats[5].radius}`, source: `${pets.cats[5].photo}`},
-                  {id: `C06`, name: `${pets.cats[6].name}`, breed: `${pets.cats[6].breed}`, distance: `${pets.cats[6].radius}`, source: `${pets.cats[6].photo}`},
-                  {id: `C07`, name: `${pets.cats[7].name}`, breed: `${pets.cats[7].breed}`, distance: `${pets.cats[7].radius}`, source: `${pets.cats[7].photo}`},
-                  {id: `C08`, name: `${pets.cats[8].name}`, breed: `${pets.cats[8].breed}`, distance: `${pets.cats[8].radius}`, source: `${pets.cats[8].photo}`},
-                  {id: `C09`, name: `${pets.cats[9].name}`, breed: `${pets.cats[9].breed}`, distance: `${pets.cats[9].radius}`, source: `${pets.cats[9].photo}`},
-                  {id: `C10`, name: `${pets.cats[10].name}`, breed: `${pets.cats[10].breed}`, distance: `${pets.cats[10].radius}`, source: `${pets.cats[10].photo}`},
-                  {id: `C11`, name: `${pets.cats[11].name}`, breed: `${pets.cats[11].breed}`, distance: `${pets.cats[11].radius}`, source: `${pets.cats[11].photo}`},
-                  {id: `C12`, name: `${pets.cats[12].name}`, breed: `${pets.cats[12].breed}`, distance: `${pets.cats[12].radius}`, source: `${pets.cats[12].photo}`},
-                  {id: `C13`, name: `${pets.cats[13].name}`, breed: `${pets.cats[13].breed}`, distance: `${pets.cats[13].radius}`, source: `${pets.cats[13].photo}`},
-                  {id: `C14`, name: `${pets.cats[14].name}`, breed: `${pets.cats[14].breed}`, distance: `${pets.cats[14].radius}`, source: `${pets.cats[14].photo}`},
-                ],
-                list: [],
-                refreshing: false,
-                currentPet: null,
-              };
-      
-            }
-
-/*
         this.state = {
+          cards: [...pets.dogs, ...pets.cats],
           list: [],
           refreshing: false,
           currentPet: null,
           
         };
-        */
+        
       }
 
       handleYup = (card) => {
